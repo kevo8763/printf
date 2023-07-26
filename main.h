a168449bc9d676772291f7727168327cc4a3c05f
kevo8763 <kipkirui8763@gmail.com>
2023-07-26 07:18:56 +0000

    putchar

--- main.h
+++ main.h
@@ -134,5 +134,7 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
+/**************putchar**********************/
+int _putchar(char c);
 
 #endif /* MAIN_H */
